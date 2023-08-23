924ad3b322f954e7aff502a193e35269e1c976ac
Nguyễn Quang Hiệp <hiepacr@gmail.com>
2023-08-23 18:13:20 +0700

    add code

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 extern crate core;
 
-use core::num::dec2flt::number;
 use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
-
-fn type_name_of_val<T>(_: T) -> &'static str {
-    std::any::type_name::<T>()
-}
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {}", secret_number);
     let mut guess = String::new();
-    println!("Guess the number!");
     println!("Please input your guess.");
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: i32 = guess.parse::<i32>().unwrap();
+    println!("Guess the number!");
+
     println!("You guessed: {guess}");
-    // println!("{}", std::any::type_name::<type_name_of_val(&guess)>());
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
